18e7b6c01d02a54da677b5e32570115ffc7641b4
richn <richn@DESKTOP-2PSF4AL>
2020-07-30 21:50:59 +0100

    Updates to images

--- src/components/SectionLocations/SectionLocations.js
+++ src/components/SectionLocations/SectionLocations.js
@@ -8,7 +8,7 @@ import { NamedLink } from '../../components';
 
 import css from './SectionLocations.css';
 
-import helsinkiImage from './images/location_helsinki.jpg';
+import helsinkiImage from './images/location_rajaampat.png';
 import rovaniemiImage from './images/location_rovaniemi.jpg';
 import rukaImage from './images/location_ruka.jpg';
 
@@ -51,9 +51,9 @@ const SectionLocations = props => {
       </div>
       <div className={css.locations}>
         {locationLink(
-          'Helsinki',
+          'Raja Ampat',
           helsinkiImage,
-          '?address=Helsinki%2C%20Finland&bounds=60.2978389%2C25.254484899999966%2C59.9224887%2C24.782875800000056&origin=60.16985569999999%2C24.93837910000002'
+          '?address=RajaAmpat%2C%20Indonesia&bounds=-74.1949%2C40.7668%2C66.1553745%2C24.736871199999996&origin=66.50394779999999%2C25.729390599999988'
         )}
         {locationLink(
           'Rovaniemi',
